3ae005867f99399591e45d1ee5bfc0701f03eb4a
Cedric Chevalier <cedric.chevalier019@proton.me>
2024-11-26 11:01:03 +0100

    Clean mat-mat

--- 01-matrix-matrix/main.c
+++ 01-matrix-matrix/main.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-void matrix_matrix_multiplication(int n, int m, int, int p, double A[n][m], double B[m][p], double C[n][p])
+void matrix_matrix_multiplication(int n, int m, int p, double A[n][m], double B[m][p], double C[n][p])
 {
     // Write the matrix-matrix multiplication here
+}
+
+
+void fill_matrix(int n, int m, double A[n][m])
+{
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < p; j++)
+        for (int k = 0; k < m; k++)
         {
-            C[i][j] = 0;
-            for (int k = 0; k < m; k++)
-            {
-                C[i][j] += A[i][k] * B[k][j];
-            }
+            A[i][k] = 1.0;
         }
     }
 }
@@ -22,39 +23,54 @@ void matrix_matrix_multiplication(int n, int m, int, int p, double A[n][m], doub
 int main(int argc, char *argv[])
 {
     int n = 100;
-    int m = 100;
-    int p = 100;
 
     // Parse dimensions of matrices
     if (argc > 1)
     {
         n = atoi(argv[1]);
     }
+
+    int m = n;
+    int p = n;
+
     if (argc > 2)
     {
         m = atoi(argv[2]);
     }
+
     if (argc > 3)
     {
         p = atoi(argv[3]);
     }
 
+    printf("Working with matrices of size %d x %d and %d x %d\n", n, m, m, p);
+
     // Allocate memory for matrices
-    double **A;
-    double **B;
-    double **C;
+    // Note how we allocate memory for a 2D array in a single block
+    // This is done to ensure that the memory is contiguous
+    // Can you comment on how data are stored in memory for this 2D array?
+    double **A = (double **)malloc(n * m * sizeof(double));
+    double **B = (double **)malloc(m * p * sizeof(double));
+    double **C = (double **)malloc(n * p * sizeof(double));
 
     // Initialize matrices
-
+    fill_matrix(n, m, A);
+    fill_matrix(m, p, B);
+    fill_matrix(n, p, C);
 
     // Call matrix_matrix_multiplication
     struct timespec start, end;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    // matrix_matrix_multiplication(n, m, p, A, B, C);
+    matrix_matrix_multiplication(n, m, p, A, B, C);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
 
     double time_taken = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1.e9;
     printf("Time taken: %e s\n", time_taken);
+
+    // Free memory
+    free(A);
+    free(B);
+    free(C);
 
     return EXIT_SUCCESS;
 }
