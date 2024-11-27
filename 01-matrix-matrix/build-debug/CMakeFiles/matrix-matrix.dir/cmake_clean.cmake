741939442b66385a2969691436dcaf3d065fc65f
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-11-27 11:10:07 +0100

    first commit

---
+++ 01-matrix-matrix/build-debug/CMakeFiles/matrix-matrix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix-matrix.dir/main.c.o"
+  "CMakeFiles/matrix-matrix.dir/main.c.o.d"
+  "matrix-matrix"
+  "matrix-matrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/matrix-matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 01-matrix-matrix/main.c
+++ 01-matrix-matrix/main.c
@@ -2,11 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
+// naive version
 void matrix_matrix_multiplication(int n, int m, int p, double A[n][m], double B[m][p], double C[n][p])
 {
-    // Write the matrix-matrix multiplication here
+    for (int i = 0; i < n; i ++)
+    {
+        for (int j = 0; j < p; j ++)
+        {
+            for (int k = 0; k < m; k ++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 }
 
+// switched version
+void matrix_matrix_multiplication2(int n, int m, int p, double A[n][m], double B[m][p], double C[n][p])
+{
+    for (int k = 0; k < m; k ++)
+    {
+        for (int i = 0; i < n; i ++)
+        {
+            for (int j = 0; j < p; j ++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
 
 void fill_matrix(int n, int m, double A[n][m])
 {
@@ -14,33 +38,45 @@ void fill_matrix(int n, int m, double A[n][m])
     {
         for (int k = 0; k < m; k++)
         {
-            A[i][k] = 1.0;
+            A[i][k] = rand() / (double)RAND_MAX;
         }
     }
 }
 
+void init_matrix(int n, int m, double A[n][m])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < m; k++)
+        {
+            A[i][k] = 0.0;
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    char *mode = argv[1];
+
     int n = 100;
 
     // Parse dimensions of matrices
-    if (argc > 1)
+    if (argc > 2)
     {
-        n = atoi(argv[1]);
+        n = atoi(argv[2]);
     }
 
     int m = n;
     int p = n;
 
-    if (argc > 2)
-    {
-        m = atoi(argv[2]);
-    }
-
     if (argc > 3)
     {
-        p = atoi(argv[3]);
+        m = atoi(argv[3]);
+    }
+
+    if (argc > 4)
+    {
+        p = atoi(argv[4]);
     }
 
     printf("Working with matrices of size %d x %d and %d x %d\n", n, m, m, p);
@@ -56,16 +92,37 @@ int main(int argc, char *argv[])
     // Initialize matrices
     fill_matrix(n, m, A);
     fill_matrix(m, p, B);
-    fill_matrix(n, p, C);
+    init_matrix(n, p, C);
 
-    // Call matrix_matrix_multiplication
-    struct timespec start, end;
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    matrix_matrix_multiplication(n, m, p, A, B, C);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
-
-    double time_taken = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1.e9;
-    printf("Time taken: %e s\n", time_taken);
+    if (mode == "naive")
+    {
+        printf("Running naive version\n");
+        
+        // Call matrix_matrix_multiplication
+        struct timespec start, end;
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+        matrix_matrix_multiplication(n, m, p, A, B, C);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        
+        double time_taken = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1.e9;
+        printf("Time taken: %e s\n", time_taken);
+    }
+    else if (mode == "switched")
+    {
+        printf("Running switched version\n");
+        // Call matrix_matrix_multiplication
+        struct timespec start, end;
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+        matrix_matrix_multiplication2(n, m, p, A, B, C);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        
+        double time_taken = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1.e9;
+        printf("Time taken: %e s\n", time_taken);
+    }
+    else
+    {
+        printf("Please indicate calculate version\n");
+    }
 
     // Free memory
     free(A);
